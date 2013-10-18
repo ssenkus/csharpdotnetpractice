c2a86b6eb01b2c8d2492c40da2cabc7910dfb28b
Steven Senkus <steven.senkus@yahoo.com>
2013-10-17 22:19:02 -0700

    "FizzBuzz" is the new "Hello World!"

---
+++ fizzbuzz/fizzbuzz/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fizzbuzz
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            bool dump = true;
+            string output;
+            for (int i = 0; i < 100; i++)
+            {
+                output = String.Format("{0}: {1}{2}", i, (i % 3 == 0) ? "Fizz" : "", (i % 5 == 0) ? "Buzz" : "");
+                Console.WriteLine(output);
+                if (i % 10 == 0 && i != 0 && dump) 
+                {
+                    derp();
+                    Console.ReadLine();
+                }
+            }
+            Console.ReadLine();
+        }
+        static void derp() 
+        {
+            Console.WriteLine("sdfsd");
+        }
+    }
+}
