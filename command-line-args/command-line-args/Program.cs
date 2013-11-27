6627470b1c7ca6fae3e237a40699f578fa8d51e6
Steven Senkus <steven.senkus@yahoo.com>
2013-11-26 22:59:34 -0800

    kinda like C!  I likes the C#

---
+++ command-line-args/command-line-args/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace command_line_args
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int len =  args.Length;
+            for (int i = 0; i < len; i++) {
+                Console.WriteLine(String.Format("{0}", args[i]));
+            }
+            Console.ReadLine();
+        }
+    }
+}
