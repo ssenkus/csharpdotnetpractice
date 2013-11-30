00c8b302bce406646ff78c0afb255dd1bf387706
Steven Senkus <steven.senkus@yahoo.com>
2013-11-29 18:22:13 -0800

    eh, some JSON request thing

---
+++ get-dat-json/getDatJSON/getDatJSON/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace getDatJSON
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string url = textBox1.Text;
+            WebRequest request = WebRequest.Create(url);
+            request.ContentType = "application/json; charset=utf-8";
+            request.Method = "GET";
+
+            
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            if (response.StatusDescription == "OK") {
+                textBox1.Enabled = false;
+                button1.Enabled = false;
+            }
+
+            Stream stream = response.GetResponseStream();
+            string json = "";
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                while (!reader.EndOfStream)
+                {
+                    json += reader.ReadLine();
+                }
+            }
+            textBox2.Text = json;
+
+        }
+
+    }
+}
