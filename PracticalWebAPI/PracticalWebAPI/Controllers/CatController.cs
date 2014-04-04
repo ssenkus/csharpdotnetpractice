263918a6870edb2dab56e93379501e913ae78f36
Steve <steven.senkus@yahoo.com>
2014-04-03 21:19:22 -0700

    quick webapi practice

---
+++ PracticalWebAPI/PracticalWebAPI/Controllers/CatController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using PracticalWebAPI.Models;
+
+namespace PracticalWebAPI.Controllers
+{
+
+    public class CatController : ApiController
+    {
+
+        private static IList<Cat> cats = new List<Cat>()
+        {
+            new Cat() {
+                id = 0,
+                name = "Grumpy",
+                age = 4,
+                breed = "Dwarfcat"
+            },
+            new Cat() {
+                id = 1,
+                name = "Henry",
+                age = 6,
+                breed = "Siamese"
+            },
+            new Cat() {
+                id = 2,
+                name = "Numnum",
+                age = 5,
+                breed = "Fatcat"
+            }
+
+        };
+
+        public IEnumerable<Cat> Get()
+        {
+            return cats;
+        }
+
+        public Cat Get(int id)
+        {
+            return cats.First(e => e.id == id);
+        }
+
+
+        
+    }
+}
---
+++ PracticalWebAPI/PracticalWebAPI/Models/Cat.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PracticalWebAPI.Models
+{
+    public class Cat
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int age { get; set; }
+        public string breed { get; set; }
+
+
+    }
+}
